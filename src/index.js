ab1f2455b3ae8842517f2a0095a30119317e3191
Kosianchuk Dmitriy <kosianchuk.d@gmail.com>
2018-02-28 23:08:05 +0200

    Add redux, auth actions, auth sagas, api, localStorage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { render } from 'react-dom'
 import React from 'react'
-
 import LoginPage from './components/LoginPage'
 import './index.css'
+import createStore from './store'
+import { Provider } from 'react-redux'
 
-render(<LoginPage />, document.getElementById('root'))
+const store = createStore()
+
+render(
+  <Provider store={store}>
+    <LoginPage />
+  </Provider>,
+  document.getElementById('root')
+)
